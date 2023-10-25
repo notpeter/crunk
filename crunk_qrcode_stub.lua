2300dcbf84f831c0ea7b01bd13f4730a24ee72a4
Peter Tripp <petertripp@gmail.com>
2023-10-25 13:21:31 -0400

    Improve LuaCATS stub.

--- crunk_qrcode_stub.lua
+++ crunk_qrcode_stub.lua
@@ -5,9 +5,9 @@ crunk = crunk or {}
 
 ---Quickly Create a QRCode
 ---@param text string
----@param ecc_level integer Error correcting duplication. -1: auto; 0: 7%, 1: 15%, 2: 25%, 3: 30%
----@param mask integer Mask pattern. -1: auto; 0-7: fixed
----@param min_version integer Minimum QRCode version (size). 1-40 (default: 1)
----@param max_version integer Maximum QRCode version (size). 1-40 (default: 40)
----@return _Image image The generated QRCode as an image (playdate.graphics.image)
+---@param ecc_level? integer Error correcting duplication. -1: auto; 0: 7%, 1: 15%, 2: 25%, 3: 30%
+---@param mask? integer Mask pattern. -1: auto; 0-7: fixed
+---@param min_version? integer Minimum QRCode version (size). 1-40 (default: 1)
+---@param max_version? integer Maximum QRCode version (size). 1-40 (default: 40)
+---@return _Image? image The generated QRCode as an image (playdate.graphics.image)
 function crunk.qrcode(text, ecc_level, mask, min_version, max_version) end
