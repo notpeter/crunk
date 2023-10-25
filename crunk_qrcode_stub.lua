889ab064a800bdf84dc4dbc86628d8c5216ba1d6
Peter Tripp <petertripp@gmail.com>
2023-10-25 12:05:40 -0400

    Initial commit of crunk_qrcode.

---
+++ crunk_qrcode_stub.lua
@@ -0,0 +1,12 @@
+---@meta
+
+local crunk = { qrcode = {} }
+
+---Quickly Create a QRCode
+---@param text string
+---@param ecc_level integer Error correcting duplication. -1: auto; 0: 7%, 1: 15%, 2: 25%, 3: 30%
+---@param mask integer Mask pattern. -1: auto; 0-7: fixed
+---@param min_version integer Minimum QRCode version (size). 1-40 (default: 1)
+---@param max_version integer Maximum QRCode version (size). 1-40 (default: 40)
+---@return _Image The generated QRCode image
+function crunk.qrcode.generate(text, ecc_level, mask, min_version, max_version) end
