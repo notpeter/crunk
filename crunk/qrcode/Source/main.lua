e7ea6c3954601bb57709be258e99ca651b4e0776
Peter Tripp <petertripp@gmail.com>
2023-12-06 11:32:01 -0500

    Remove box drawing.

--- crunk/qrcode/Source/main.lua
+++ crunk/qrcode/Source/main.lua
@@ -15,15 +15,7 @@ local function setup()
         error("Failed to generate QRCode")
     end
     print("Generated QRCode in " .. elapsed .. "ms")
-    -- image:drawScaled(20, 20, 4, 4)
-
-    local image2 = crunk.qrcode.generate("https://panic.com", 0, 3, nil, 9, 9)
-    if image2 == nil then
-        error("Failed to generate QRCode")
-    end
-    image2:drawScaled(0, 0, 4, 4)
-    local size = image2.width
-    playdate.graphics.fillRect(0 + size * 1.3, 0 + size * 1.3, size * 1.4, size * 1.4)
+    image:drawScaled(20, 20, 4, 4)
 
     playdate.update = update
 end
